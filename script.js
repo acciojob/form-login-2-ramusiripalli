cb4154bb0c2647139f395ef162443f64bf2b1092
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-30 11:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 //your JS code here. If required.
-function showAlert(){
+function showAlert(event){
+	event.preventDefault();
 	const firstName = document.getElementById("firstName").value;
 	const lastName = document.getElementById("lastName").value;
 	const phoneNumber = document.getElementById("phoneNumber").value;
 	const emailId = document.getElementById("emailId").value;
 
-	alert(`First Name : ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`)
+	alert(`First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`)
 }
