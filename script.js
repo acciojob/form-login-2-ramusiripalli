80e06e804192282ce102ed088ca08f469e81d3de
ramu siripalli <ramsiripalli2772@gmail.com>
2026-06-30 11:25:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function showAlert(){
+	const firstName = document.getElementById("firstName").value;
+	const lastName = document.getElementById("lastName").value;
+	const phoneNumber = document.getElementById("phoneNumber").value;
+	const emailId = document.getElementById("emailId").value;
+
+	alert(`First Name : ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`)
+}
